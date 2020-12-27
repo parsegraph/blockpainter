d970caefdd0271f808e7247b7f4378c3459b2309
Aaron Faanes <dafrito@gmail.com>
2020-12-27 01:17:08 -0600

    More tests

--- test/test.js
+++ test/test.js
@@ -1,12 +1,30 @@
 var assert = require("assert");
 import BlockPainter from "../dist/blockpainter";
 import {BasicGLProvider} from 'parsegraph-compileprogram';
+import Color from 'parsegraph-color';
 
 import {mockDOM} from 'node-canvas-webgl';
 mockDOM(window);
 
-describe("Package", function () {
+describe("BlockPainter", function () {
   it("works", ()=>{
+    let ctx = new BasicGLProvider();
+    ctx.setExplicitSize(400, 400);
+    const bp = new BlockPainter(ctx);
+    assert.ok(bp);
+    bp.initBuffer(2);
+    bp.drawBlock(.5, .5, .5, .25, .5, .5, .5);
+    bp.drawBlock(.5, .5, .25, .25, .5, .5, .5);
+    const gl = ctx.gl();
+    gl.enable(gl.BLEND);
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+    ctx.container().style.width = "100%";
+    ctx.container().style.height = "100%";
+    gl.viewport(0, 0, 400, 400);
+    ctx.render();
+    bp.render([1, 0, 0, 0, 1, 0, 0, 0, 1], 1000.0, false);
+  });
+  it("has a simple mode", ()=>{
     let ctx = new BasicGLProvider();
     ctx.setExplicitSize(400, 400);
     const bp = new BlockPainter(ctx);
@@ -20,6 +38,99 @@ describe("Package", function () {
     ctx.container().style.height = "100%";
     gl.viewport(0, 0, 400, 400);
     ctx.render();
-    bp.render([1, 0, 0, 0, 1, 0, 0, 0, 1], 1000.0, false);
+    bp.render([1, 0, 0, 0, 1, 0, 0, 0, 1], 1000.0, true);
+  });
+  it("has idempotent initBuffer", ()=>{
+    let ctx = new BasicGLProvider();
+    ctx.setExplicitSize(400, 400);
+    const bp = new BlockPainter(ctx);
+    assert.ok(bp);
+    bp.initBuffer(1);
+    bp.initBuffer(1);
+    bp.drawBlock(.5, .5, .25, .25, .5, .5, .5);
+    const gl = ctx.gl();
+    gl.enable(gl.BLEND);
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+    ctx.container().style.width = "100%";
+    ctx.container().style.height = "100%";
+    gl.viewport(0, 0, 400, 400);
+    ctx.render();
+    bp.render([1, 0, 0, 0, 1, 0, 0, 0, 1], 1000.0, true);
+  });
+  it("can be cleared", ()=>{
+    let ctx = new BasicGLProvider();
+    ctx.setExplicitSize(400, 400);
+    const bp = new BlockPainter(ctx);
+    assert.ok(bp);
+    bp.initBuffer(1);
+    bp.drawBlock(.5, .5, .25, .25, .5, .5, .5);
+    bp.clear();
+    bp.initBuffer(1);
+    const gl = ctx.gl();
+    gl.enable(gl.BLEND);
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+    ctx.container().style.width = "100%";
+    ctx.container().style.height = "100%";
+    gl.viewport(0, 0, 400, 400);
+    ctx.render();
+    bp.render([1, 0, 0, 0, 1, 0, 0, 0, 1], 1000.0, true);
+  });
+  it("can be resized", ()=>{
+    let ctx = new BasicGLProvider();
+    ctx.setExplicitSize(400, 400);
+    const bp = new BlockPainter(ctx);
+    assert.ok(bp);
+    bp.initBuffer(1);
+    bp.drawBlock(.5, .5, .25, .25, .5, .5, .5);
+    bp.initBuffer(2);
+    const gl = ctx.gl();
+    gl.enable(gl.BLEND);
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+    ctx.container().style.width = "100%";
+    ctx.container().style.height = "100%";
+    gl.viewport(0, 0, 400, 400);
+    ctx.render();
+    bp.render([1, 0, 0, 0, 1, 0, 0, 0, 1], 1000.0, true);
+  });
+  it("can be changed in color", ()=>{
+    let ctx = new BasicGLProvider();
+    ctx.setExplicitSize(400, 400);
+    const bp = new BlockPainter(ctx);
+    assert.ok(bp);
+    bp.initBuffer(1);
+    bp.drawBlock(.5, .5, .25, .25, .5, .5, .5);
+    bp.initBuffer(2);
+    const gl = ctx.gl();
+    gl.enable(gl.BLEND);
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+    ctx.container().style.width = "100%";
+    ctx.container().style.height = "100%";
+    bp.setBackgroundColor(new Color(1, 1, 0, 1));
+    bp.setBorderColor(new Color(1, 0, 0, 1));
+    gl.viewport(0, 0, 400, 400);
+    ctx.render();
+    bp.render([1, 0, 0, 0, 1, 0, 0, 0, 1], 1000.0, true);
+  });
+  it("keeps track of its own bounds", ()=>{
+    let ctx = new BasicGLProvider();
+    ctx.setExplicitSize(400, 400);
+    const bp = new BlockPainter(ctx);
+    bp.initBuffer(2);
+    bp.drawBlock(.5, .5, .25, .25, .5, .5, .5);
+    assert.ok(bp.bounds());
+    bp.drawBlock(.5, .5, .25, .25, .5, .5, .5);
+    assert.ok(bp.bounds());
+  });
+  it("has an ID", ()=>{
+    let ctx = new BasicGLProvider();
+    ctx.setExplicitSize(400, 400);
+    const bp = new BlockPainter(ctx);
+    assert.ok(bp.id());
+  });
+  it("supports toString", ()=>{
+    let ctx = new BasicGLProvider();
+    ctx.setExplicitSize(400, 400);
+    const bp = new BlockPainter(ctx);
+    assert.ok(bp.toString());
   });
 });
