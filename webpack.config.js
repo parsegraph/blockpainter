76ec207d64b37bc5015e6c41b485f66ef0cc75c7
Aaron Faanes <dafrito@gmail.com>
2022-02-26 21:18:35 -0600

    Upgrade webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,9 @@
-const path = require("path");
+const {webpackConfig, relDir} = require("./webpack.common");
 
 module.exports = {
-  externals: {
-    "parsegraph-checkglerror":{
-      commonjs:"parsegraph-checkglerror",
-      commonjs2:"parsegraph-checkglerror",
-      amd:"parsegraph-checkglerror",
-      root:"parsegraph_checkglerror"
-    }
-  },
   entry: {
-    lib: path.resolve(__dirname, "src/blockpainter.ts"),
-    demo: path.resolve(__dirname, "src/demo.ts"),
+    lib: relDir("src/blockpainter.ts"),
+    demo: relDir("src/demo.ts"),
   },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "parsegraph-blockpainter.[name].js",
-    globalObject: "this",
-    library: "parsegraph_blockpainter",
-    libraryTarget: "umd",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|ts|tsx?)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader', 'ts-loader']
-      },
-      {
-        test: /\.(glsl|vs|fs|vert|frag)$/,
-        exclude: /node_modules/,
-        use: ["ts-shader-loader"],
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".js", ".ts", ".tsx", ".glsl"],
-    modules: [
-      path.resolve(__dirname, "src"),
-      path.resolve(__dirname, "node_modules"),
-    ]
-  },
-  mode: "development",
-  devtool: "eval-source-map",
+  ...webpackConfig(false),
 };
